1f71e73152116bd2aa73d8f51a28150b0fcf988a
RSPHenrik <rickard.pettersten@yh.nackademin.se>
2022-11-16 21:32:20 +0100

    Bug fixin

---
+++ Dark_Secret_Project/Assets/Rickard/Scripts/FuseGameManager.cs
@@ -0,0 +1,125 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuseGameManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject fuseBlockOne;
+    [SerializeField]
+    GameObject fuseBlockTwo;
+    [SerializeField]
+    GameObject fuseBlockThree;
+
+    bool finish = false;
+
+    int[,] spawnDecider = new int[4, 4]
+    {
+        {0,1,1,0 },
+        {2,0,0,2 },
+        {0,1,2,0 },
+        {0,2,0,1 }
+    };
+
+    [SerializeField]
+    PowerBlock[,] powerBlocks = new PowerBlock[4, 4];
+
+    void Start()
+    {
+        for (int y = 0;  y < spawnDecider.GetLength(0);  y++)
+        {
+            for (int x = 0; x < spawnDecider.GetLength(1); x++)
+            {
+                MakeAChild(spawnDecider[y, x], y, x);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void MakeAChild(int index, int y, int x)
+    {
+        GameObject a = Instantiate(TypeSelctor(index), new Vector3(0, 0, 0), Quaternion.identity);
+        a.transform.parent = gameObject.transform;
+        a.transform.localRotation = Quaternion.Euler(Vector3.forward * -90);
+        a.transform.localPosition = new Vector3(0, y * -0.06f, x * 0.06f);
+        powerBlocks[y, x] = a.GetComponent<PowerBlock>();
+        powerBlocks[y, x].AssigneManager(this);
+        powerBlocks[y, x].gridLocalX = x;
+        powerBlocks[y, x].gridLocalY = y;
+    }
+
+    private GameObject TypeSelctor(int index)
+    {
+        if (index == 0)
+            return fuseBlockOne;
+        if (index == 1)
+            return fuseBlockTwo;
+        else
+            return fuseBlockThree;
+    }
+
+    public void UpdateBox()
+    {
+        Debug.Log("Updating Box Grid");
+        BlockLoop();
+        if (finish)
+            Debug.Log("You won!");
+    }
+
+    public void Snaking()
+    {
+        List<Vector2> path = new List<Vector2>();
+        path.Add(new Vector2(0, 2));
+
+        for (int i = 0; i < 4*4; i++)
+        {
+            for (int p = 0; p < 4; p++)
+            {
+                Vector2 currentLoc = path[i];
+                if(currentLoc.y != 0 && p == 0)
+                {
+                    powerBlocks[Mathf.RoundToInt(currentLoc.y), Mathf.RoundToInt(currentLoc.x)].SnakeCheck(powerBlocks[Mathf.RoundToInt(currentLoc.y) - 1, Mathf.RoundToInt(currentLoc.x)], 0);
+                }
+                if(currentLoc.x != powerBlocks.GetLength(1) && p==1)
+                {
+
+                }
+            }
+        }
+    }
+
+    private void BlockLoop()
+    {
+        for (int y = 0; y < powerBlocks.GetLength(0); y++)
+        {
+            for (int x = 0; x < powerBlocks.GetLength(1); x++)
+            {
+                if (y != 0)
+                    powerBlocks[y, x].CheckUp(powerBlocks[y - 1, x]);
+                if (y != powerBlocks.GetLength(0) - 1)
+                    powerBlocks[y, x].CheckDown(powerBlocks[y + 1, x]);
+                if (x != 0)
+                    powerBlocks[y, x].CheckLeft(powerBlocks[y, x - 1]);
+                if (x != powerBlocks.GetLength(1) - 1)
+                    powerBlocks[y, x].CheckRight(powerBlocks[y, x + 1]);
+
+
+                
+
+                if (y == 2 && x == 0)
+                    powerBlocks[y, x].CheckStart();
+
+                if (y == 0 && x == powerBlocks.GetLength(1) - 1)
+                    finish = powerBlocks[y, x].CheckFinish();
+
+            }
+        }
+
+
+    }
+}
--- Dark_Secret_Project/Assets/Rickard/Scripts/PowerBlock.cs
+++ Dark_Secret_Project/Assets/Rickard/Scripts/PowerBlock.cs
@@ -8,8 +8,17 @@ public class PowerBlock : MonoBehaviour
     [SerializeField]
     bool[] gate = new bool[4];
 
+    bool[] powerdGate = new bool[4] { false, false, false, false };
+
     [SerializeField]
     bool powered = false;
+
+    public int gridLocalX;
+    public int gridLocalY;
+
+
+
+    FuseGameManager  fuseGameManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,32 +31,145 @@ public class PowerBlock : MonoBehaviour
             return;
     }
 
+    public void OnPush()
+    {
+        RotateBlock();
+        fuseGameManager.UpdateBox();
+    }
     public void RotateBlock()
     {
         bool pGate = false;
-        Debug.Log(pGate);
+        
         for (int i = 0; i < gate.Length; i++)
         {
             if (gate[i] && !pGate)
             {
                 pGate = true;
                 gate[i] = false;
-                Debug.Log(pGate);
+                
             }
             else if(!gate[i] && pGate)
             {
                 pGate = false;
                 gate[i] = true;
-                Debug.Log(pGate);
+                
             }
             if(i == gate.Length -1 && pGate)
             {
                 pGate = false;
                 gate[0] = true;
-                Debug.Log(pGate);
+                
             }
-            Debug.Log(i + " is: " + gate[i]);
+            
         }
         thisGameObject.transform.Rotate(Vector3.up * 90.0f);
+    }
+
+    public void AssigneManager(FuseGameManager fGM)
+    {
+        fuseGameManager = fGM;
+    }
+
+    public bool CheckStart()
+    {
+        if (gate[3])
+            return true;
+        else
+            return false;
+    }
+
+    public bool CheckFinish()
+    {
+        if (gate[1] && powerdGate[2])
+            return true;
+        else
+            return false;
+    }
+
+    public void CheckUp(PowerBlock pBlock)
+    {
+        Debug.Log("Checking up");
+        if (pBlock.CheckGate(2) && gate[0] && powered)
+        {
+            pBlock.PowerUP(true);
+            Debug.Log("Checking up, Got POWER on Gate 0" + " X = " + gridLocalX + " Y = " + gridLocalY);
+        }
+        else if(powered)
+            pBlock.PowerUP(false);
+    }
+    public void CheckRight(PowerBlock pBlock)
+    {
+        Debug.Log("Checking Right");
+        if (pBlock.CheckGate(3) && gate[1] && powered)
+        {
+            pBlock.PowerUP(true);
+            Debug.Log("Checking Right, Got POWER on Gate 1" + " X = " + gridLocalX + " Y = " + gridLocalY);
+        }
+        else if(powered)
+            pBlock.PowerUP(false);
+    }
+    public void CheckDown(PowerBlock pBlock)
+    {
+        Debug.Log("Checking Down");
+        if (pBlock.CheckGate(0) && gate[2] && powered)
+        {
+            pBlock.PowerUP(true);
+            Debug.Log("Checking Down, Got POWER on Gate 2" + " X = " + gridLocalX + " Y = " + gridLocalY);
+        }
+        else if (powered)
+            pBlock.PowerUP(false);
+    }
+    public void CheckLeft(PowerBlock pBlock)
+    {
+        Debug.Log("Checking Left");
+        if (pBlock.CheckGate(1) && gate[3] && powered)
+        {
+            pBlock.PowerUP(true);
+            Debug.Log("Checking Left, Got POWER on Gate 3" + " X = " + gridLocalX + " Y = " + gridLocalY);
+        }
+        else if (powered)
+            pBlock.PowerUP(false);
+    }
+
+    public void ResolvePoweredStatus()
+    {
+        int poweredGates = 0;
+        for (int i = 0; i < powerdGate.Length; i++)
+        {
+            if (powerdGate[i])
+                poweredGates++;
+        }
+        if (poweredGates < 2 && powered)
+        {
+            powered = false;
+        }
+        else if (poweredGates == 1 && !powered)
+            powered = true;
+        
+    }
+
+    public bool SnakeCheck(PowerBlock pBlock, int pathIndex)
+    {
+        int reverseIndex = pathIndex < 2 ? pathIndex + 2 : pathIndex - 2;
+        if (gate[pathIndex] && pBlock.CheckGate(reverseIndex))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+
+    public bool CheckGate(int index)
+    {
+        return gate[index];
+    }
+    public bool CheckPowered()
+    {
+        return powered;
+    }
+
+    public void PowerUP(bool value)
+    {
+        powered = value;
     }
 }
