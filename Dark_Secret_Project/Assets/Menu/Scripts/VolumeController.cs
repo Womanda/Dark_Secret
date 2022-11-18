b843325d4e14f5c3eb1ab5a24a7ada0a5b22b905
Amanda Norlén <Amanda.Norlen@yh.nackademin.se>
2022-11-18 18:15:03 +0100

    Phone working
    Phone now fully works

--- Dark_Secret_Project/Assets/Menu/Scripts/VolumeController.cs
+++ Dark_Secret_Project/Assets/Menu/Scripts/VolumeController.cs
@@ -9,7 +9,7 @@ public class VolumeController : MonoBehaviour
     [Header("Volume setting")]
     [SerializeField] private Text volumeTextValue = null;
     [SerializeField] private Slider volumeSlider = null;
-    [SerializeField] private float defaultVolume = 1.0f;
+    [SerializeField] private float defaultVolume = 0.5f;
 
     [Header("Confirmation")]
     [SerializeField] private GameObject confirmationPrompt = null;
@@ -20,13 +20,13 @@ public class VolumeController : MonoBehaviour
         volumeTextValue.text = volume.ToString("0.0");
     }
 
-    public void VolumeApply()
+    public void VolumeApply() // Confirmar att volymkontrollen sparas i spelet
     {
         PlayerPrefs.SetFloat("masterVolume", AudioListener.volume);
         StartCoroutine(ConfirmationBox());
     }
 
-    public IEnumerator ConfirmationBox() // Confirmar att volymkontrollen sparas i spelet
+    public IEnumerator ConfirmationBox()
     {
         confirmationPrompt.SetActive(true);
         yield return new WaitForSeconds(2);
