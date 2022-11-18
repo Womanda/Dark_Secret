380d0e1e30024e97625bf56492dae52b98de1634
Landerino <max.hellenberg@yh.nackademin.se>
2022-11-18 03:01:16 +0100

    BallGame Finished
    Finished scripting BallGame, it looks very primitive but for now it's good.

--- Dark_Secret_Project/Assets/AA Max/Scripts/BallGameScript.cs
+++ Dark_Secret_Project/Assets/AA Max/Scripts/BallGameScript.cs
@@ -4,21 +4,102 @@ using UnityEngine;
 
 public class BallGameScript : MonoBehaviour
 {
+    //Mr. Boll
     public GameObject Ball;
-    public GameObject SpotOne;
+    //nummer bollen står på
+    private int currentNumber;
+    //lägg till nummer
+    private int AddNumber;
+    //Antal slots (dont touch!)
+    public GameObject[] Slots;
 
     void Start()
     {
-        List<Slots> slotPos = new List<Slots>();
+        currentNumber = 1;
 
-        slotPos.Add(new)
-
-        Ball.transform.SetParent(SpotOne.transform);
-        Ball.transform.localPosition = new Vector3(0, 0, 0);
+        //Så att bollen startar på rätt plats
+        UpdateBallPos();
     }
 
     public void moveBallRight()
     {
+        if(currentNumber == 9)
+        {
+            currentNumber += 3;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 10 || currentNumber == 13)
+        {
+            currentNumber += 2;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 1 || currentNumber == 3)
+        {
+            currentNumber += 1;
+            UpdateBallPos();
+        }
 
     }
+    public void moveBallDown()
+    {
+        if (currentNumber == 15)
+        {
+            currentNumber -= 12;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 13 || currentNumber == 10)
+        {
+            currentNumber -= 8;
+            UpdateBallPos();
+        }
+        else if (currentNumber ==  9)
+        {
+            currentNumber -= 4;
+            UpdateBallPos();
+        }
+    }
+    public void moveBallLeft()
+    {
+        if (currentNumber == 12)
+        {
+            currentNumber -= 3;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 15)
+        {
+            currentNumber -= 2;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 4 || currentNumber == 2 || currentNumber == 10)
+        {
+            currentNumber -= 1;
+            UpdateBallPos();
+        }
+    }
+    public void moveBallUp()
+    {
+        if (currentNumber == 3)
+        {
+            currentNumber += 12;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 2 || currentNumber == 5)
+        {
+            currentNumber += 8;
+            UpdateBallPos();
+        }
+        else if (currentNumber == 9 || currentNumber == 4)
+        {
+            currentNumber += 4;
+            UpdateBallPos();
+        }
+    }
+
+    //parentar bollen till nummer och uppdaterar sedan bollens position till rätt ställen
+    void UpdateBallPos()
+    {
+        Ball.transform.SetParent(Slots[currentNumber].transform);
+        Ball.transform.localPosition = new Vector3(0, 0, 0);
+    }
+
 }
