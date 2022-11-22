06b6c1f3720d2dabaa242b475517e54da4d2b973
Landerino <max.hellenberg@yh.nackademin.se>
2022-11-22 20:38:03 +0100

    PadGame Timer
    New timer added to PadGame

--- Dark_Secret_Project/Assets/AA Max/Scripts/PadPuzzle.cs
+++ Dark_Secret_Project/Assets/AA Max/Scripts/PadPuzzle.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PadPuzzle : MonoBehaviour
 {
@@ -11,17 +12,18 @@ public class PadPuzzle : MonoBehaviour
     //gömm clockan först och sätt sedan på den vid första rätta intryck
     public GameObject clock;
 
+
+    //Timer grejer
+    public int Duration { get; private set; }
+    private int remainingDuration;
+    //Grab Clock 
+    public Text text;
+
     void Start()
     {
         clock.SetActive(false);
         clockActive = false;
         padPuzzleProgress = 0;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     public void puzzleProgress()
@@ -30,6 +32,8 @@ public class PadPuzzle : MonoBehaviour
         {
             clockActive = true;
             clock.SetActive(true);
+            SetDuration(8);
+            Begin();
         }
         padPuzzleProgress++;
     }
@@ -37,6 +41,51 @@ public class PadPuzzle : MonoBehaviour
     public void puzzleReset()
     {
         padPuzzleProgress = 0;
+    }
+
+    public PadPuzzle SetDuration(int seconds)
+    {
+        Duration = remainingDuration = seconds;
+        return this;
+    }
+
+    //starta klockan
+    public void Begin()
+    {
+        StartCoroutine(UpdateTimer());
+    }
+
+    //uppdaterar klockan varje sekund när klockan är 0 call timerranout
+    private IEnumerator UpdateTimer()
+    {
+        while (remainingDuration > 0)
+        {
+            UpdateUI(remainingDuration);
+            remainingDuration--;
+            yield return new WaitForSeconds(1f);
+        }
+        TimerRanOut();
+    }
+
+    //Formaterar texten till ett minuter:sekunder format
+    private void UpdateUI(int seconds)
+    {
+        text.text = string.Format("{0:D2}:{1:D2}", seconds / 60, seconds % 60);
+    }
+
+    //När tiden tagit slut naturally gör följande
+    public void TimerRanOut()
+    {
+        Debug.Log("Timer ran out");
+        text.text = "00:00";
+        Duration = remainingDuration = 0;
+    }
+
+    //om du löser puzzlet innan tiden tagit slut
+    public void winConditionMet()
+    {
+        Debug.Log("you won the padgame!");
+        StopAllCoroutines();
     }
 
 }
